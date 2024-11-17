8693f5ee4bf1bf8d98ad15dedab66fe57d9e27b4
Getnett <getnetendaylalu2015@gmail.com>
2024-11-17 23:20:21 +0300

    Init Mispay test project

---
+++ README.md
@@ -0,0 +1,6 @@
+Mis Pay test project
+
+# Goal
+
+create an app that shows visualizations of data.The data displayed will be fetched from NASA’s NeoWs (Near Earth Object Web
+Service)
---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1 className="text-3xl font-bold underline">
+        Start Mis Pay test project!
+      </h1>
+    </>
+  );
+}
+
+export default App;
