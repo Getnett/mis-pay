0bb3d6ce7c98c67b2f55bd00c11427822ef404de
Getnett <getnetendaylalu2015@gmail.com>
2024-11-18 02:54:20 +0300

    Add dropdown selector

--- src/types/nasaNEO.ts
+++ src/types/nasaNEO.ts
@@ -3,6 +3,4 @@ export interface NasaNEO {
   estimatedDiameterMin: number;
   estimatedDiameterMax: number;
   orbitingBody: string;
-  // "estimated_diameter_min": 22.0067027115,
-  // "estimated_diameter_max": 49.2084832235
 }
